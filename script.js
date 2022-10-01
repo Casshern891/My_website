f6c1ba4a9b064b5d6c0694caa8476e3c75667f92
Richard Lovell <cassh3rn@talktalk.net>
2022-10-01 17:50:25 +0100

    add loop for adding text element

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const newPara = document.createElement('p');
-newPara.textContent = "Welcome traveller";
-document.body.prepend(newPara);
+
+for (let i = 0; i < 10; i++) {
+    
+    const newPara = document.createElement('p');
+    newPara.textContent = "Welcome traveller";
+    document.body.prepend(newPara);
+    if (i % 2 == 0) {
+        newPara.style.setProperty('color', 'red');
+    } else {
+        newPara.style.setProperty('color', 'green');
+    }
+}
