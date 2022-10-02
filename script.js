519e4d77947dc0e7400e045d3dba42c6e1e11a70
Richard Lovell <cassh3rn@talktalk.net>
2022-10-02 20:28:44 +0100

    add toggle-switch.css and remove test js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 
-for (let i = 0; i < 10; i++) {
-    
-    const newPara = document.createElement('p');
-    newPara.textContent = "Welcome traveller";
-    document.body.prepend(newPara);
-    if (i % 2 == 0) {
-        newPara.style.setProperty('color', 'red');
-    } else {
-        newPara.style.setProperty('color', 'green');
-    }
-}
